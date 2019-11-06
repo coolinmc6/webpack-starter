b9f8c243b44da0344f64c5864bff099eaaf31aec
Colin McNamara <coolinmc6@gmail.com>
2019-11-06 11:17:01 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
-import './styles.scss';
-import messi1 from './messi1.gif';
-import messi2 from './messi2.png';
-import messi3 from './messi3.jpg';
+import './css/styles.scss';
 
-
-
-const Sup = () => {
+const App = () => {
     return (
-        <div>
-            <h1>Sup Bra?</h1>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fuga excepturi sunt deserunt, voluptatibus soluta labore saepe, omnis velit veniam ratione explicabo sapiente doloribus quas delectus quam obcaecati. Eligendi, id.</div>
-            <img src={messi1} />
-            <img src={messi2} />
-            <img src={messi3} />
-        </div>
+        <main>
+            <h1>Main App</h1>
+        </main>
     )
 }
 
-render(<Sup />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
